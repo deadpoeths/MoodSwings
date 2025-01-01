c908c7ca00a4fb76588deb40a233d16f210d06b9
aleenaahmed21 <aleenaahmed142002@gmail.com>
2025-01-01 16:07:53 +0500

    fix: fixed vercel deployment issues

--- backend/Server.js
+++ backend/Server.js
@@ -7,11 +7,22 @@ const moodRoutes = require("./Routes/moodRoutes");
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:3000", // Development URL
+  "https://moodswings.vercel.app", // Production URL
+];
+
 const corsOptions = {
-    origin: "https://moodswings.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  };
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); // Allow the request
+    } else {
+      callback(new Error("Not allowed by CORS")); // Block the request
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 app.use(cors(corsOptions));
 app.use(express.json());
 
